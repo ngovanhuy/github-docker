8b321d24369a871f8329435d56698d47836d9761
NgoVanHuy <ngovanhuy.cntt2@gmail.com>
2017-08-25 00:47:33 +0700

    change home page

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Hello Docker World New";
     }
 
     public static void main(String[] args) {
