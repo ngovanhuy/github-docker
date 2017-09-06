98b63c2bff311748a42966faa1e6b8a71330a334
NgoVanHuy <ngovanhuy.cntt2@gmail.com>
2017-09-06 11:26:00 +0700

    update 4

--- src/main/java/hello/Application.java
+++ src/main/java/hello/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World New 3";
+        return "Hello Docker World New 4";
     }
 
     public static void main(String[] args) {
